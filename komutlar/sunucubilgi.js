3fe13d03e0e5cbed89f1b6c543b4690671a9f36f
barbarbar338 <35371155+barbarbar338@users.noreply.github.com>
2018-05-23 14:54:51 +0300

    Update sunucubilgi.js

--- komutlar/sunucubilgi.js
+++ komutlar/sunucubilgi.js
@@ -10,7 +10,8 @@ exports.run = (client, message, params) => {
     .setAuthor(message.author.username, message.author.avatarURL)
     .addField(':warning: Uyarı :warning:', '`sunucubilgi` adlı komutu özel mesajlarda kullanamazsın.')
     return message.author.send(ozelmesajuyari); }
-    if (message.channel.type !== 'dm') {
+    if (message.channel.type !== 'dm') {      
+	var sunucu = message.guild;
       const sunucubilgi = new Discord.RichEmbed()
     .setColor(0x00AE86)
     .setTimestamp()
@@ -18,13 +19,16 @@ exports.run = (client, message, params) => {
     .addField('Ad:', message.guild.name)
     .addField('ID', message.guild.id)
     .addField('Ana kanal:', message.guild.defaultChannel)
+	.addField("Yazı Kanalları", sunucu.channels.filter(c => c.type === "text").size, true)
+    .addField("Ses Kanalları", sunucu.channels.filter(c => c.type === "voice").size, true)
     .addField('Bölge', message.guild.region)
     .addField('Üye sayısı:', message.guild.memberCount)
+	.addField("Çevrimiçi", sunucu.members.filter(m => m.user.presence.status === "online").size, true)
     .addField('Sahibi:', message.guild.owner + ' (' + message.guild.ownerID + ')')
     .addField('Kanal sayısı:', message.guild.channels.size)
     .addField('Oluşturulma tarihi:', message.guild.createdAt)
     return message.channel.send(sunucubilgi);
-    }
+				       }
 };
 
 exports.conf = {
