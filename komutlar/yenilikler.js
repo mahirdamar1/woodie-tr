b735162da2938f301e91bb32595e79885729031a
barbarbar338 <35371155+barbarbar338@users.noreply.github.com>
2018-06-10 13:59:24 +0300

    Update yenilikler.js

--- komutlar/yenilikler.js
+++ komutlar/yenilikler.js
@@ -8,7 +8,19 @@ exports.run = function(client, message) {
     .setDescription("Yenilik her zaman iyidir")
     .setThumbnail("http://fantasyva.com/wp-content/uploads/2017/08/new-sign-ID-33314.png")
     .setColor(0x00AE86)
-    .addField('Müzik komutları eklendi: ', `-help müzik yazarak daha fazla bilgi edinin!`)
+    .addField('Müzik komutları eklendi: ', `Artık botumuzla müzik dinleyebileceksiniz ^^.`)
+    .addField("Müzik Komutları: ", "-help müzik")
+    .setDescription(`**-join**:Mesajı gönderenin kanalına katılır.
+**-leave**:Mesajı gönderenin kanalından ayrılır.
+**-add**: Link/Şarkı adı ile sıraya şarkı ekler.
+**-queue**:Kuyruğu gösterir.
+**-play**:Sıradaki şarkıyı oynatır.
+:AŞAĞIDAKI KOMUTLAR YALNIZCA PLAY KOMUTU ÇALIŞIYORKEN ÇALIŞIR:
+**-pause**:Müziği duraklatır.
+**-resume**:Müziği sürdürür.
+**-skip**:Çalmakta olan şarkıyı atlar.
+**-volume+(+++)**:Ses şiddetini 2%/+ kadar arttırır.
+**-volume-(---)**:Ses şiddetini 2%/- kadar azaltır.`)
     message.channel.send(embed);
 
 };
