19663715943ffcbcb9edbe763ecaff50c61b922d
barbarbar338 <35371155+barbarbar338@users.noreply.github.com>
2018-06-19 20:59:02 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,7 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   
-  client.user.setGame(`-öneri | -yenilikler | -yardım | 3.0.0 sürümü ile`, 'https://www.twitch.tv/xgodpro');
+  client.user.setGame(`-öneri | -yenilikler | -help | 3.0.0 sürümü ile`, 'https://www.twitch.tv/xgodpro');
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
   console.log("[BOT]Hazır");
