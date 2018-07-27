a3d8e3c36e165e35a7da26fdfacb74a632299f83
barbarbar338 <35371155+barbarbar338@users.noreply.github.com>
2018-07-27 14:56:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -110,7 +110,7 @@ client.on("message", msg => {
 		return;
 	}else if (msg.content.toLowerCase().match(/(discord\.gg\/)|(discordapp\.com\/invite\/)/g) && !msg.author.bot && msg.channel.type === "text" && msg.channel.permissionsFor(msg.guild.member(client.user)).has("MANAGE_MESSAGES")) {
     msg.delete(30).then(deletedMsg => {
-     deletedMsg.channel.send(":crown: "msg.author + " Reklam koruması aktif, reklam yapmayı bırak evlat!:crown:")
+     deletedMsg.channel.send(":crown: " + msg.author + " Reklam koruması aktif, reklam yapmayı bırak evlat!:crown:")
 	 msg.guild.owner.send(msg.guild.owner + "Merhaba seni uyarmam gerekiyor. Sunucunda " + msg.author.tag + " reklam yapıyor!").catch(e => {
             console.error(e);
           });
