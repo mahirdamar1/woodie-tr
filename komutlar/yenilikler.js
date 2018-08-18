5e42e81d048423630a44a061991f5aeac2d42199
barbarbar338 <35371155+barbarbar338@users.noreply.github.com>
2018-08-18 16:42:59 +0300

    Update yenilikler.js

--- komutlar/yenilikler.js
+++ komutlar/yenilikler.js
@@ -8,11 +8,13 @@ exports.run = function(client, message) {
     .setDescription("Yenilik her zaman iyidir")
     .setThumbnail("http://fantasyva.com/wp-content/uploads/2017/08/new-sign-ID-33314.png")
     .setColor(0x00AE86)
-    .addField("Tic-Tac-Toe (xox) Oyunu Eklendi: ", "Artık botumuz aracılığı ile bir arkadaşınızla xox oyunu oynayabileceksiniz -help xox")
-    .setDescription("-tictactoe <@kullanıcı_id> ile arkadaşınız ve sizin arnızda bir oyun başlatın, sıra size gelince -işaretle <satır> <sıra> komutu ile bir işaretleme yap! Sıra ve satırların koordinatları 0 ile 2 arasındadır örnek: -işaretle 0 0 veya -işaretle 2 2")
+    .addField("RPG (MOB) Oyunu Eklendi: ", "Artık botumuzun oluşturduğu moblarla savaşabileceksiniz -help rpg")
+.setDescription(`-başla Komutu ile moblarımızla savaşa başlayabilirsiniz! Kullanabileceğiniz saldırılar: saldır, ateştopu, can, em. Şuanda sadece eğlence amaçlıdır gelecekte para sistemiyle paranızı ikiye katlayabileceksiniz!`)
     message.channel.send(embed);
 
 };
+
+
 
 exports.conf = {
   enabled: true, 
