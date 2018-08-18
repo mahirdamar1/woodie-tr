18847c3d4c029cd997cc393655be2131fbd38609
barbarbar338 <35371155+barbarbar338@users.noreply.github.com>
2018-08-18 16:55:56 +0300

    Update yenilikler.js

--- komutlar/yenilikler.js
+++ komutlar/yenilikler.js
@@ -9,7 +9,7 @@ exports.run = function(client, message) {
     .setThumbnail("http://fantasyva.com/wp-content/uploads/2017/08/new-sign-ID-33314.png")
     .setColor(0x00AE86)
     .addField("RPG (MOB) Oyunu Eklendi: ", "Artık botumuzun oluşturduğu moblarla savaşabileceksiniz -help rpg")
-.setDescription(`-başla Komutu ile moblarımızla savaşa başlayabilirsiniz! Kullanabileceğiniz saldırılar: saldır, ateştopu, can, em. Şuanda sadece eğlence amaçlıdır gelecekte para sistemiyle paranızı ikiye katlayabileceksiniz!`)
+.setDescription(`-başla Komutu ile moblarımızla savaşa başlayabilirsiniz! Kullanabileceğiniz saldırılar: saldır, ateştopu, can, em. -bitir Komutuyla savaşı bitirebilirsiniz (doğal olarak kaybedersiniz). Şuanda sadece eğlence amaçlıdır gelecekte para sistemiyle paranızı ikiye katlayabileceksiniz!`)
     message.channel.send(embed);
 
 };
